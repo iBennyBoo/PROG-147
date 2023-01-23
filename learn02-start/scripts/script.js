b8da49815ec8f6c3115786f03a7768af595f92fe
BennyBoo <bpb062004@gmail.com>
2023-01-23 16:08:08 -0600

    Add files via upload

---
+++ learn02-start/scripts/script.js
@@ -0,0 +1,9 @@
+function myFunction2(){
+        document.getElementById("sample").innerHTML = "Changed from a function in the scripts folder";
+    }
+
+    // single line comment
+
+    /*
+    block comment | multi-line comment
+    */
